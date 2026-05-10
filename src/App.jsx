889322e1b7d4d52bc96c4ae4d8d550288e36aef3
rhmatt14_ <rahmattulah267@gmail.com>
2026-05-10 17:08:31 +0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Login from './Login'; 
+import Login from './login'; 
 import SAWCalculator from './SAWCalculator'; // Ini file hitung-hitungan SPK lu
 
 function App() {
